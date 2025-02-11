bb15a04f176558d84022161f01cc2c827aa9e185
Caramelo <cccarlos@iesrafaelalberti.es>
2025-02-11 20:11:21 +0100

    Mejora de codigo y depuracion

--- src/Main.kt
+++ src/Main.kt
@@ -5,6 +5,6 @@ fun main() {
     var rectangulo = Rectangulo("Rectangulo",color = Color.colorAleatorio(),8.0,20.0)
     println(rectangulo)
 
-    var triangulo = Triangulo("Triangulo",color = Color.colorAleatorio(),8.0,5.0,9.0,6.0,4.0)
+    var triangulo = Triangulo("Triangulo",color = Color.colorAleatorio(),8.0,5.0,9.0,6.0)
     println(triangulo)
 }
